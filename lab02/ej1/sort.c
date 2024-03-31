500d4a12f9f1161ee307e7f72f770e27d2baa062
imanol <imanolsarnago@gmail.com>
2024-03-31 17:15:13 -0300

    final programs

--- lab02/ej1/sort.c
+++ lab02/ej1/sort.c
@@ -8,38 +8,19 @@
 
 
 static void insert(int a[], unsigned int i) {
-    /* needs implementation */
-
-    /*proc insert (in/out a: array[1..n] of T, in i: nat)
-        var j: nat
-        j:= i
-        do j > 1 ∧ a[j] < a[j − 1] → swap(a,j-1,j)
-        j:= j-1
-        od
-    end proc*/
 
     unsigned int j =i;
-    while (j>1 && a[j]<a[j-1]){
+    while (j>0 && goes_before(a[j],a[j-1])){
         swap(a,j-1,j);
         j = j-1;
     }
-
-
 }
 
 void insertion_sort(int a[], unsigned int length) {
 
-    /*Invariante:
-    el arreglo a es una permutación del original y
-    un segmento inicial a[1,i) del arreglo está ordenado.
-    (pero en general a[1,i) no contiene los mínimos del arreglo)
-*/
-
-
-
     for (unsigned int i = 1u; i < length; ++i) {
         /* needs implementation */
+        assert(array_is_sorted(a,i));
         insert(a, i);
-        assert(array_is_sorted);
     }
 }
